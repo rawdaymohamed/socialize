b715505e78c3b278da2bc70bb280dea6806da89f
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-12 01:58:15 +0200

    Feat: Add authentication using clerk

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import React from "react";
 import MobileMenu from "./MobileMenu";
-import { FaHome, FaUserFriends } from "react-icons/fa";
+import { FaHome, FaRegUserCircle, FaUserFriends } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { IoPeople } from "react-icons/io5";
+import { FaRegMessage } from "react-icons/fa6";
+import { IoMdNotifications } from "react-icons/io";
+
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
@@ -32,8 +37,26 @@ const Navbar = () => {
       </div>
       {/* Right */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <ClerkLoading>
+          <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid  border-e-transparent  align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="flex items-center gap-7 cursor-pointer">
+              <IoPeople className="size-6 text-gray-600" />
+              <FaRegMessage className="size-4 text-gray-600" />
+              <IoMdNotifications className="size-6 text-gray-600" />
+              <UserButton/>
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center gap-2 text-gray-600">
+              <FaRegUserCircle className="size-5" />
+              <Link href="/sign-in" className="text-sm">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
-        
       </div>
     </div>
   );
