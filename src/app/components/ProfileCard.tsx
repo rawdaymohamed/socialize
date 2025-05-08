be1bdc771d834216354223638b3eba2997a791b5
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-05-09 01:33:13 +0300

    Feat: Add followers to ProfileCard

--- src/app/components/ProfileCard.tsx
+++ src/app/components/ProfileCard.tsx
@@ -8,9 +8,17 @@ const ProfileCard = () => {
         <Image fill src="/abstract-1.jpg" className='rounded-md object-cover' alt='abstract' />
         <Image src="/abstract-2.jpg" width={48} height={48} className='rounded-full w-12 h-12  object-cover absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10' alt='abstract' />
       </div>
-      <div className='h-20 flex flex-col gap-2 items-center'>
+      <div className='h-32 flex flex-col gap-2 items-center'>
         <h1 className='font-semibold'>Rawda Yasser</h1>
-        <div className='flex items-center gap-4'></div>
+        <div className='flex items-center gap-5'>
+          <div className='flex gap-2'>
+            <Image width={24} height={24} src="/abstract-1.jpg" className='rounded-full object-cover h-6 w-6' alt='abstract' />
+            <Image width={24} height={24} src="/abstract-1.jpg" className='rounded-full object-cover h-6 w-6' alt='abstract' />
+            <Image width={24} height={24} src="/abstract-1.jpg" className='rounded-full object-cover h-6 w-6' alt='abstract' />
+          </div>
+        </div>
+          <span className='text-gray-800 font-semibold text-xs'>500 followers</span>
+          <button className='bg-blue-600 text-white py-2 px-3 rounded-md hover:bg-blue-800 transition cursor-pointer'>My Profile</button>
       </div>
     </div>
   )
